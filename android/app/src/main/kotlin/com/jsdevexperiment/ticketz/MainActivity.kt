eb5b832a190d50e39cdf1e449c6ca726d6acee52
Jun Siong <junsiong2599@gmail.com>
2022-08-11 00:20:13 +0800

    1.1.0(2) Added Crashlytics and App Check

--- android/app/src/main/kotlin/com/jsdevexperiment/ticketz/MainActivity.kt
+++ android/app/src/main/kotlin/com/jsdevexperiment/ticketz/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.jsdevexperiment.ticketz
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.AppCheckProviderFactory;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+
 
 class MainActivity: FlutterActivity() {
 }
