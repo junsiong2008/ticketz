324d81abb07ab9b663cf4d9f564033a0eaf47157
Teng Jun Siong <junsiong2599@gmail.com>
2024-04-13 23:41:32 +0800

    resync

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -25,10 +25,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -65,6 +62,16 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyB5-5EDtPBjplqWTiGvA4uFZdqhF--LfDE',
+    appId: '1:926544606239:ios:14bef092d06263e80e912a',
+    messagingSenderId: '926544606239',
+    projectId: 'cw-bsmm-2022',
+    storageBucket: 'cw-bsmm-2022.appspot.com',
+    iosClientId: '926544606239-2ncije9gmhgsv268nuke8a6u5mv8ejql.apps.googleusercontent.com',
+    iosBundleId: 'com.jsdevexperiment.ticketz',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
     apiKey: 'AIzaSyB5-5EDtPBjplqWTiGvA4uFZdqhF--LfDE',
     appId: '1:926544606239:ios:14bef092d06263e80e912a',
     messagingSenderId: '926544606239',
